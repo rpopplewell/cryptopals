88fe1e37843df491c06715a8134931e8d5b03737
TheDemx27 <chrisdickerman.main@gmail.com>
2024-03-05 15:59:01 -0500

    random key encryption

--- src/aes.rs
+++ src/aes.rs
@@ -1,11 +1,13 @@
-use itertools::Itertools;
-use openssl::cipher;
-use openssl::symm::{decrypt, encrypt, Cipher};
 use base64::prelude::*;
+use itertools::{concat, Itertools};
+use openssl::symm::{decrypt, encrypt, Cipher};
+use openssl::{cipher, rand::rand_bytes};
+use rand::distributions::Uniform;
+use rand::{thread_rng, Rng};
 use std::clone;
 use std::fs::File;
 use std::io::{self, Read};
-use std::ops::Sub;
+use std::ops::{Rem, Sub};
 use std::path::Path;
 use xor::{self, XOR};
 
@@ -15,10 +17,36 @@ pub enum EncryptDecrypt {
     Decrypt,
 }
 
+fn rand_bytes_len(len: usize) -> Vec<u8> {
+    let mut buf = Vec::with_capacity(len);
+    rand_bytes(&mut buf).unwrap();
+    buf.to_vec()
+}
+
+fn rand_padding(input: &[u8]) -> Vec<u8> {
+    let mut rng = thread_rng();
+    let padding_lengths: Vec<usize> = rng.sample_iter(Uniform::new(5, 10)).take(2).collect();
+    let rand_padding: Vec<Vec<u8>> = padding_lengths.iter().map(|pl: &usize| rand_bytes_len(*pl)).collect();
+    concat([rand_padding[0].clone(), input.to_vec(), rand_padding[1].clone()])
+}
+
+fn encrypt_with_rand_key(input: &[u8]) -> Vec<u8> {
+    let padded_input = rand_padding(input);
+    let key = rand_bytes_len(16);
+    let res: Vec<u8>;
+    if 1 == rand::random::<u8>().rem(2) {
+        res = aes_128_ecb(padded_input.as_slice(), &key, EncryptDecrypt::Encrypt)
+    } else {
+        let iv = rand_bytes_len(16);
+        res = aes_128_cbc(padded_input.as_slice(), &key, iv, EncryptDecrypt::Encrypt)
+    }
+    res
+}
+
 pub fn aes_128_ecb(input: &[u8], key: &[u8], ed: EncryptDecrypt) -> Vec<u8> {
     let res = match ed {
-        EncryptDecrypt::Encrypt => { encrypt(Cipher::aes_128_ecb(), key, None, input).unwrap() }
-        EncryptDecrypt::Decrypt => { decrypt(Cipher::aes_128_ecb(), key, None, input).unwrap() }
+        EncryptDecrypt::Encrypt => encrypt(Cipher::aes_128_ecb(), key, None, input).unwrap(),
+        EncryptDecrypt::Decrypt => decrypt(Cipher::aes_128_ecb(), key, None, input).unwrap(),
     };
     return res;
 }
@@ -30,7 +58,7 @@ pub fn aes_128_cbc(input: &[u8], key: &[u8], iv: Vec<u8>, ed: EncryptDecrypt) ->
 
     let mut iv_xor: Vec<u8> = iv;
     let mut acc: Vec<u8> = Vec::<u8>::new();
-    
+
     if ed == EncryptDecrypt::Decrypt {
         for cipher_chunk in chunks {
             let plain_chunk = aes_128_ecb(&cipher_chunk, key, ed);
@@ -46,7 +74,7 @@ pub fn aes_128_cbc(input: &[u8], key: &[u8], iv: Vec<u8>, ed: EncryptDecrypt) ->
             iv_xor = cipher_chunk;
         }
     }
-    
+
     acc
 }
 
@@ -57,9 +85,7 @@ pub fn pkcs_7_padding(bytes: &[u8], blocksize: usize) -> Vec<u8> {
     let padding = vec![padding_size as u8; padding_size];
     rem.extend_from_slice(padding.as_slice());
 
-    let mut padded_bytes: Vec<u8> = chunks.flat_map(
-        |chunk| chunk.iter().copied()
-    ).collect();
+    let mut padded_bytes: Vec<u8> = chunks.flat_map(|chunk| chunk.iter().copied()).collect();
 
     padded_bytes.extend_from_slice(&rem);
     return padded_bytes;
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,18 @@
 #![allow(unused)]
 mod aes;
-use openssl::error::Error;
-use openssl::symm::{decrypt, encrypt, Cipher};
 use base64::prelude::*;
 use core::fmt;
+use openssl::error::Error;
+use openssl::symm::{decrypt, encrypt, Cipher};
 use std::fs::File;
+use std::io::Read;
 use std::io::{self, BufRead};
 use std::ops::Rem;
 use std::path::Path;
-use std::io::Read;
 
 fn get_input() -> String {
     let path = "./inputs/10.txt";
-    let mut reader = io::BufReader::new(
-        File::open(
-            &Path::new(path)
-        ).unwrap()
-    );
+    let mut reader = io::BufReader::new(File::open(&Path::new(path)).unwrap());
     let mut buffer = Vec::<u8>::new();
     let _ = reader.read_to_end(&mut buffer);
     String::from_utf8(buffer).unwrap().replace("\n", "")
@@ -33,13 +29,18 @@ fn main() {
     // let res = decrypt(Cipher::aes_128_cbc(), key, Some(iv.as_slice()), cipher_text.as_bytes()).unwrap();
 
     let mut res = Vec::<u8>::new();
-    match decrypt(Cipher::aes_128_cbc(), key, Some(iv), cipher_bytes.as_slice()) {
+    match decrypt(
+        Cipher::aes_128_cbc(),
+        key,
+        Some(iv),
+        cipher_bytes.as_slice(),
+    ) {
         Ok(x) => res = x,
         Err(err) => println!("{:?}", err),
     }
 
     let ans = String::from_utf8(res).unwrap().replace("\n", "");
-    
+
     println!("{:?}", ans)
     // println!("{:?}", res);
 
